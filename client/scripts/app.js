159595029c8d4ebb881c03dc99d960d9ae4342d3
lizportnoy <lizport10@gmail.com>
2014-11-10 15:37:56 -0800

    Messages fetched from the server display.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,41 @@
-// YOUR CODE HERE:
 
+// Suggested from Readme
+var getMessages = function () {$.ajax({
+  // always use this url
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  // data: '',
+  contentType: 'application/json',
+  success: function (data) {
+    parseMessages(data);
+    console.log(data);
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to get messages');
+  }
+});
+};
+
+
+var parseMessages = function(allMessages){
+  _.each(allMessages.results, function(msg){
+    var output = '<li>' + msg.username + ': ' + msg.text + '@' + msg.createdAt + '</li>';
+    $('.messages').prepend(html_sanitize(output));
+    while ($('.messages').children().length >20) {
+      $('.messages').children().last().remove();
+    }
+  });
+};
+
+setInterval(getMessages, 1000);
+
+
+// var escape = function(s) {
+//   return s.split('#').map(function(v) {
+//       // Only 20% of slashes are end tags; save 1.2% of total
+//       // bytes by only escaping those.
+//       var json = JSON.stringify(v).replace(/<\//g, '<\\/');
+//       return json;
+//       }).join('');
+// }
